f15d2a1f946d3898996f99768993f5869f893a97
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2023-02-08 00:54:30 -0300

    att new pc

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password:'postgres',
+    password:'123456',
     database: 'codeburger',
     define: {
         timespamps: true,
