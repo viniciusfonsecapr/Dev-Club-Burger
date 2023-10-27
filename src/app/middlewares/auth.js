4c113fd4afec84c4870e76b521e85b0ad8f89547
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2023-10-27 00:39:00 -0300

    pt.5

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,12 +1,11 @@
 
 import jsonwebtoken from 'jsonwebtoken'
+
 import authConfig from '../../config/auth.js'
 
 export default (request, response, next) => {
   const authToken = request.headers.authorization
 
-  
- 
   if (!authToken) {
     return response.status(401).json({ error: 'Token not provided' })
   }
